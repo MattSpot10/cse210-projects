713e37f0285eae9b4b52e5bcdab98616671dc68f
Matthew Spotten <48339420+MattSpot10@users.noreply.github.com>
2025-04-24 11:16:17 -0600

    prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Input your first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Input your last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your first name is: {firstName}");
+        Console.WriteLine($"Your last name is: {lastName}");
+
     }
 }
