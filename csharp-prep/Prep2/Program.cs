e17b0122ee3b42622f68386ddee39f81f41e4269
Matthew Spotten <48339420+MattSpot10@users.noreply.github.com>
2025-04-26 21:57:50 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade?: ");
+
+        string grade = Console.ReadLine();
+        float percent = float.Parse(grade);
+        string letterGrade = "";
+
+        if (percent >= 90) {
+            letterGrade = "A";
+        }
+        else if (percent >= 80) {
+            letterGrade = "B";
+        }
+        else if (percent >= 70) {
+            letterGrade = "C";
+        }
+        else if (percent >= 60) {
+            letterGrade = "D";
+        }
+        else if (percent < 60 ) {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your Grade is a {letterGrade}");
+        if (percent > 70) {
+            Console.WriteLine("Congradualtions you passed your class!");
+        }
+        else {
+            Console.WriteLine("Unfortunately you didn't pass but there is always next time!");
+        }
     }
 }
