a86301e7a0689d86eac6f11872ef0d5d9f5e7315
Matthew Spotten <48339420+MattSpot10@users.noreply.github.com>
2025-05-30 19:16:14 -0600

    fixed bugs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -10,24 +10,28 @@ class Program
             new Scripture("Alma 32:21", "And now, as I said concerning faith—faith is not to have a perfect knowledge of things; therefore if ye have faith ye hope for things which are not seen, which are true."),
             new Scripture("Mosiah 3:19", "For the natural man is an enemy to God, and has been from the fall of Adam, and will be, forever and ever, unless he yields to the enticings of the Holy Spirit, and putteth off the natural man and becometh a saint through the atonement of Christ the Lord, and endureth all things while being humble and meek, and submissive, patient, full of love, willing to submit to all things which the Lord seeth fit to inflict upon him, even as a child doth submit to his father."),
         };
-        string input = "";
-        while (input != "q")
+        while (true)
         {
             Console.Clear();
             for (int i = 0; i < scptrs.Count(); i++)
             {
-
                 Console.WriteLine($"{i + 1}: {scptrs[i].GetScriptureReference()}");
             }
             Console.WriteLine("q: to quit");
 
+
             int scptrIndex = -1;
-            if (int.TryParse(Console.ReadLine(), out scptrIndex))
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out scptrIndex))
             {
                 scptrIndex -= 1;
             }
             else
             {
+                if (input == "q")
+                {
+                    break;
+                }
                 Console.WriteLine("Invalid input. Please enter a number.");
             }
             if (0 <= scptrIndex && scptrIndex < scptrs.Count())
