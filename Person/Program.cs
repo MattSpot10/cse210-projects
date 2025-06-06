753723d5b114c9190928fdfb9df848b62a1878a5
Matthew Spotten <48339420+MattSpot10@users.noreply.github.com>
2025-06-05 20:52:40 -0600

    completed learning04

--- Person/Program.cs
+++ Person/Program.cs
@@ -3,5 +3,15 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World");
+        Person myPerson = new Person("John", "Doe", 56);
+
+        PoliceMan myPoliceMan = new PoliceMan("Betty", "Crocker", 23, "bat");
+
+        Doctor myDoctor = new Doctor("Bob", "Payne", 43, "Knife");
+
+        Console.WriteLine(myPerson.GetPersonInformation());
+
+        Console.WriteLine(myPoliceMan.GetPoliceManInformation());
+        Console.WriteLine(myDoctor.GetDoctorInformation());
     }
 }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment myAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment myMathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        WritingAssignment myWritingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+
+        Console.WriteLine(myAssignment.GetSummary());
+        Console.WriteLine();
+        Console.WriteLine(myMathAssignment.GetHomeworkList());
+        Console.WriteLine();
+        Console.WriteLine(myWritingAssignment.GetWritingInformation());
+
+
+
+        // Console.WriteLine("Hello Learning04 World!");
     }
 }
